24b9bcbb5cd18b723ea4155976f4066bf00df183
Nijesh stha <162810084+NijeshShtha@users.noreply.github.com>
2026-06-20 21:31:05 +0545

    resource group and storage account created in azure using terraform

---
+++ storageaccount/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 4.1.0"
+    } 
+  }
+  required_version = ">= 1.15.6"
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "nijeshanishstorage7"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
